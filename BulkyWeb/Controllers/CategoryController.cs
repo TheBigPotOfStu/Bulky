72addbc4d2828eb9fe439474cc705234b701104e
TheBigPotOfStu <my.stu.lies@gmail.com>
2024-11-21 10:14:17 +1300

    Page up and running. 3 basic categories defined.

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
